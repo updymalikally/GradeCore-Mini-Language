da03a5f5ce672b99df67e671cbab2896c03e4f60
updymalikally <zalow62@gmail.com>
2026-05-18 06:55:30 +0300

    Implemented add student command in interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,2 +1,35 @@
 class GradeCoreInterpreter:
-    pass
+
+    def __init__(self):
+        self.students = {}
+
+    def execute(self, code):
+
+        statements = code.split(';')
+
+        for stmt in statements:
+
+            stmt = stmt.strip()
+
+            if not stmt:
+                continue
+
+            parts = stmt.split()
+
+            # ADD STUDENT
+            if parts[0].upper() == "ADD" and parts[1].upper() == "STUDENT":
+
+                name = parts[2]
+
+                self.students[name] = None
+
+                print(f"Student {name} added.")
+
+
+program = """
+ADD STUDENT John;
+"""
+
+interpreter = GradeCoreInterpreter()
+
+interpreter.execute(program)
